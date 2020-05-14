7021e69f881a87d52420f3f496270da0e08785ff
Seth Ockerman <ockermas@mail.gvsu.edu>
2020-05-14 16:36:54 -0400

    Create Solution.java

---
+++ TwoSumSolution/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // the array to return 
+        int[] solution = new int[2];
+        //find the indexes of the desired numbers to create the sum
+        for(int i = 0; i < nums.length; ++i){
+            for(int j = i + 1; j < nums.length; ++j){
+                if(nums[i] + nums[j] == target){
+                    solution[0] = i;
+                    solution[1] = j;
+                }
+            }
+        }
+        return solution;
+    }     
+}
