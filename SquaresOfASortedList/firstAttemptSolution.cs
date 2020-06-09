6c5d671f2299ec7a2ca545efffd3db805dd67ec8
Seth Ockerman <ockermas@mail.gvsu.edu>
2020-06-09 16:11:00 -0400

    Create firstAttemptSolution.cs

---
+++ SquaresOfASortedList/firstAttemptSolution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    // TODO there likely is a way to do it in one pass; current solution O(Nlog(N))
+    public int[] SortedSquares(int[] A) {
+        // square each number in the list: O(n)
+        for(int i = 0; i < A.GetLength(0); ++i){
+            A[i] = A[i] * A[i];
+        }
+        // sort using built in library for log(n) sorting
+        Array.Sort(A);
+        // return squared and sorted list
+        return A;
+    }
+}
