ffb3c763feae966c871eb0bd4395c5929c0a1583
Seth Ockerman <ockermas@mail.gvsu.edu>
2020-08-22 23:16:37 -0400

    Create Solution.java

---
+++ codingBatSolutions/Array3/linearIn/Solution.java
@@ -0,0 +1,24 @@
+public boolean linearIn(int[] outer, int[] inner) {
+  int in = 0;
+  int out = 0;
+  int count = 0;
+  
+  while(in < inner.length){
+    if(inner[in] == outer[out]){
+      ++count;
+      ++in;
+      out = 0;
+    }
+    
+    else if(out == outer.length - 1){
+      ++in;
+      out = 0;
+    }
+    
+    else{
+      ++out;
+    }
+  }
+  return count == inner.length;
+  
+}
